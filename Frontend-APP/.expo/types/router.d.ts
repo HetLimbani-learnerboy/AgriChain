0d8826ce127a8720415977ca3380f0cd46820be3
anuj11122005 <anujraval.11122005@gmail.com>
2025-09-10 15:13:26 +0530

    edited setup

--- Frontend-APP/.expo/types/router.d.ts
+++ Frontend-APP/.expo/types/router.d.ts
@@ -4,8 +4,9 @@ import * as Router from 'expo-router';
 export * from 'expo-router';
 
 declare module 'expo-router' {
-  
+
   export namespace ExpoRouter {
+    
     export interface __routes<T extends string | object = string> {
       hrefInputParams: { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; };
       hrefOutputParams: { pathname: Router.RelativePathString, params?: Router.UnknownOutputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownOutputParams } | { pathname: `/`; params?: Router.UnknownOutputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams; };
