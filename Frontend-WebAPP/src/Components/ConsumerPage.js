9f67a2d387e85cf9340e6d28e50cf81e62678378
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-09-19 21:49:48 +0530

    Add

--- Frontend-WebAPP/src/Components/ConsumerPage.js
+++ Frontend-WebAPP/src/Components/ConsumerPage.js
@@ -19,10 +19,6 @@ const ConsumerPage = () => {
 
   return (
     <div className="consumer-dashboard-webapp">
-<<<<<<< HEAD
-      {/* Navbar */}
-=======
->>>>>>> eb662a97909fbc95ed6f8ad210a068697deff7ad
       <div className="navigator-bar-container">
         <img src="/MainLogo.png" alt="logo" className="logo" />
         <span>AgriChain</span>
@@ -55,11 +51,7 @@ const ConsumerPage = () => {
                 Farmer
               </button>
 
-<<<<<<< HEAD
-              <button role="menuitem" onClick={() => { setDropdownOpen(false); navigate("/distributorpage");}}>
-=======
               <button role="menuitem" onClick={() => { setDropdownOpen(false); navigate("/distributorpage"); }}>
->>>>>>> eb662a97909fbc95ed6f8ad210a068697deff7ad
                 Distributor
               </button>
               <button role="menuitem" onClick={() => { setDropdownOpen(false); navigate("/retailerpage"); }}>
